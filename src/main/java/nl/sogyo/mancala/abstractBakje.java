8a33c02ef7cdf500468ee48179431efa8ad1f0f7
skorenberg <skorenberg@sogyo.nl>
2017-01-31 15:05:24 +0100

    Mancala

--- src/main/java/nl/sogyo/mancala/abstractBakje.java
+++ src/main/java/nl/sogyo/mancala/abstractBakje.java
@@ -1,10 +1,65 @@
 package nl.sogyo.mancala;
 
-public class AbstractBakje {
+public abstract class abstractBakje {
     private int steentjes;
     private Speler eigenaar;
+    private abstractBakje volgendBakje;
 
-    public int getSteentjes() {
+    //
+    // Constructor
+    //
+    protected abstractBakje(int steentjes, abstractBakje volgendBakje, Speler eigenaar) {
+        this.steentjes = steentjes;
+        this.volgendBakje = volgendBakje;
+        this.eigenaar = eigenaar;
+    }
+
+    //
+    // Verbind de bakjes van beide spelers.
+    //
+    protected void verbindBakjes(abstractBakje anderBakje) {
+        this.getKalaha().volgendBakje = anderBakje;
+        anderBakje.getKalaha().volgendBakje = this;
+    }
+
+    //
+    // Getters
+    //
+    protected int getSteentjes() {
         return steentjes;
     }
+    protected abstractBakje getVolgendBakje() {
+        return volgendBakje;
+    }
+    protected abstractBakje getVolgendBakje(int stapjes) {
+        while (stapjes<1) stapjes += 14;        // Zodat negatieve stapjes ook werken. 14 stapjes is een rondje op het bord.
+        if (stapjes == 1) {
+            return volgendBakje;
+        }
+        else {
+            return volgendBakje.getVolgendBakje(stapjes-1);
+        }
+    }
+    protected Speler getEigenaar() {
+        return eigenaar;
+    }
+
+    //
+    // Pas het aantal steentjes aan.
+    //
+    protected void leeg() {
+        this.steentjes = 0;
+    }
+    protected void voegSteentjeToe() {
+        this.steentjes++;
+    }
+
+    //
+    // Abstracte methodes
+    //
+    protected abstract abstractBakje getKalaha();
+    protected abstract void voegGestolenSteentjesToeAanKalaha(int aantal);
+    protected abstract void geefDoor(int aantal);
+    protected abstract boolean dezeEnAlleVolgendeLeeg();
+    protected abstract void actieBijLaatsteSteentje();
 }
